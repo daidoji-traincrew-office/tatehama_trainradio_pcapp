5d779d5c967212cca5bf68cdf62a234e9a7198e9
Turara_Hoshikoma <admin@novatrix-pj.com>
2025-07-02 20:36:45 +0900

    初期状態

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace tatehama_trainradio_pcapp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
